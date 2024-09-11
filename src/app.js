363f155a175a7dd8520dd72d4135cbcfc4789b76
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-09-11 23:18:24 +0700

    tao dbmongoose

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const app = express()
+const connection = require('./config/database');
+const mongoose = require('mongoose');
+const { name } = require('ejs');
+
+const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
@@ -16,4 +20,22 @@ configViewEngine(app);
 //create route
 app.use('/', webRoutes);
 
-app.listen(port,hostname)
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'Lenhnhapxuat' });
+cat.save();
+
+(async() => {
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`App listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log("Error connect to DB: ",error)
+   }
+    
+})()
+
