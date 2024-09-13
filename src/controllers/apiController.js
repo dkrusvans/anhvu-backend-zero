9c27424d5eedf090b679e47708176f9ab70fd9ff
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-09-13 19:32:14 +0700

    finish

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -41,8 +41,63 @@ const postCreateLnxAPI = async (req, res) => {
         data: lnx
     })
 }
+const putUpdateUserAPI = async (req, res) => {
+    
+    let { userId, username, email, password } = req.body;
+    let user = await User.updateOne(
+        { _id: userId },
+        { username: username },
+        { email: email},
+        { password: password}
+    );
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    })
+}
+const putUpdateLnxAPI = async (req, res) => {
+    
+    let { lnxId, khachhang, loainhapxuat, thoigiannhanhang,
+        tenvattu, quycach, soluong, trongluong, dongia, cbkd } = req.body;
+    let lnx = await Lnx.updateOne(
+        { _id: lnxId },
+        { khachhang: khachhang },
+        { loainhapxuat: loainhapxuat },
+        { thoigiannhanhang: thoigiannhanhang },
+        { tenvattu: tenvattu },
+        { quycach: quycach },
+        { soluong: soluong },
+        { trongluong: trongluong },
+        { dongia: dongia },
+        { cbkd: cbkd }
+    );
+    
+    return res.status(200).json({
+        errorCode: 0,
+        data: lnx
+    })
+}
+const deleteUserAPI = async (req,res) => {
+    const id = req.body.userId;
+    let result = await User.deleteOne({ _id:id })
+    
+    return res.status(200).json({
+        errorCode: 0,
+        data: result
+    })
+}
+const deleteLnxAPI = async (req,res) => {
+    const id = req.body.lnxId;
+    let result = await Lnx.deleteOne({ _id:id })
+    
+    return res.status(200).json({
+        errorCode: 0,
+        data: result
+    })
+}
 
 module.exports = {
     getUsersAPI, postCreateUserAPI, getLenhnhapxuatsAPI,
-    postCreateLnxAPI
+    postCreateLnxAPI, putUpdateUserAPI, putUpdateLnxAPI,
+    deleteUserAPI, deleteLnxAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routerAPI = express.Router()
 const { getUsersAPI, postCreateUserAPI, getLenhnhapxuatsAPI,
-    postCreateLnxAPI } = require('../controllers/apiController')
+    postCreateLnxAPI, putUpdateUserAPI, putUpdateLnxAPI,
+    deleteUserAPI, deleteLnxAPI
+    } = require('../controllers/apiController')
 
 
 routerAPI.get('/users', getUsersAPI);
 routerAPI.get('/lenhnhapxuat', getLenhnhapxuatsAPI);
 routerAPI.post('/users', postCreateUserAPI);
 routerAPI.post('/lenhnhapxuat', postCreateLnxAPI);
+routerAPI.put('/users', putUpdateUserAPI);
+routerAPI.put('/lenhnhapxuat', putUpdateLnxAPI);
+routerAPI.delete('/users', deleteUserAPI);
+routerAPI.delete('/lenhnhapxuat', deleteLnxAPI);
   
 module.exports = routerAPI;
