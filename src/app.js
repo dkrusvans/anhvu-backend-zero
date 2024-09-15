71f042fb289bf104dd465cc78c409b8ac0fc5cb7
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-09-16 00:18:24 +0700

    mongodb driver

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
+
 const connection = require('./config/database');
+const { MongoClient } = require('mongodb');
+
 const fileupload = require('express-fileupload');
 // const { name } = require('ejs');
 
@@ -27,7 +30,19 @@ app.use('/v1/api/', apiRoutes);
 
 (async() => {
     try {
-        await connection();
+        // Connection URL using mongedb driver
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+        // Database Name
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('documents');
+        
+        // using mongoose
+        // await connection();
         app.listen(port, hostname, () => {
             console.log(`App listening on port ${port}`)
         })
